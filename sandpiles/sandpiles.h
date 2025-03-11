30dd7f8099153c028d3b70f876761e2ca8392fbb
TuyenH <thtuyen199@gmail.com>
2025-03-11 22:02:22 +0000

    commit header file

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef SANDPILES
+#define SANDPILES
+
+void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
+int is_stable(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void stablize_sandpile(int grid[3][3]);
+
+#endif
