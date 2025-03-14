5091c966dbdeb998cf6ae37bc7deba549c18963a
TuyenH <thtuyen199@gmail.com>
2025-03-14 09:30:59 +0100

    update header file

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,10 +1,6 @@
-#ifndef SANDPILES
-#define SANDPILES
+#ifndef SANDPILES_H
+#define SANDPILES_H
 
-void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
-int is_stable(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void stablize_sandpile(int grid[3][3]);
 
-#endif
+#endif /* SANDPILES_H */
